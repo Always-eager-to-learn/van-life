d9f23f6ee8d21dd4b06a6ededad727671fb375f8
Always-eager-to-learn <avinodaaron06@gmail.com>
2025-07-10 17:45:48 +0530

    Changed the style of login and layout for desktop and laptop screens

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -8,11 +8,13 @@ export default function About(){
             <img src={PersonImage} alt="Image of a person on a van" />
             <section className={styles.about_info}>
                 <h1 className='font-big'>Don't squeeze in a sedan when you could relax in a van.</h1>
+            </section>
+            <section>
                 <p className='font-medium'>
                     Our mission is to enliven your road trip with the perfect travel van rental. Our vans are recertified before each trip to ensure your travel plans can go off without a hitch.<br />
                     (Hitch costs extra 😉)
-
-                    <br /><br />
+                </p>
+                <p className={`font-medium ${styles.second_column}`}>
                     Our team is full of vanlife enthusiasts who know firsthand the magic of touring the world on 4 wheels.
                 </p>
             </section>
